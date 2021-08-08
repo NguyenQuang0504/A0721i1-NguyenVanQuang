607a28453bb416bb90c93ead4e1409eb404fc46a
NguyenQuang0504 <quang10a30504@gmail.com>
2021-08-08 07:42:55 +0700

    up bai 9

---
+++ modul 1/bai_9_cau_truc_dieu_kien_2/bai_tap/GamePuzzle/main.js
@@ -0,0 +1,73 @@
+let a = 0;
+let b = 0;
+let c = 0;
+let d=0;
+function img1() {
+    a=a+1;
+    if (a===1) {
+        document.getElementById('img1').src = 'img/img2.1.jpg'
+    }
+    else if(a===2){
+        document.getElementById('img1').src = 'img/img3.1.jpg'
+    }
+    else{
+        document.getElementById('img1').src = 'img/img1.1.jpg'
+    }
+    if(a>=3){
+        a=0;
+    }
+}
+function img2() {
+    b=b+1;
+    if (b===1) {
+        document.getElementById('img2').src = 'img/img2.2.jpg'
+    }
+    else if(b===2){
+        document.getElementById('img2').src = 'img/img3.2.jpg'
+    }
+    else{
+        document.getElementById('img2').src = 'img/img1.2.jpg'
+    }
+    if(b>=3){
+        b=0;
+    }
+}
+function img3() {
+    c=c+1;
+    if (c===1) {
+        document.getElementById('img3').src = 'img/img2.3.jpg'
+    }
+    else if(c===2){
+        document.getElementById('img3').src = 'img/img3.3.jpg'
+    }
+    else{
+        document.getElementById('img3').src = 'img/img1.3.jpg'
+    }
+    if(c>=3){
+        c=0;
+    }
+}
+function img4() {
+    d=d+1;
+    if (d===1) {
+        document.getElementById('img4').src = 'img/img2.4.jpg'
+    }
+    else if(d===2){
+        document.getElementById('img4').src = 'img/img3.4.jpg'
+    }
+    else{
+        document.getElementById('img4').src = 'img/img1.4.jpg'
+    }
+    if(d>=3){
+        d=0;
+    }
+}
+function result1() {
+    if(a===b&&b===c&&c===d){
+        document.getElementById('result').innerText = 'Chinh Xac!'
+    }
+    else{
+        document.getElementById('result').innerText = 'Rat tiec, ban hay thu lai!';
+    }
+}
+
---
+++ modul 1/bai_9_cau_truc_dieu_kien_2/bai_tap/UngDungVuiDoYouLoveMe/main.js
@@ -0,0 +1,10 @@
+function noH()
+{
+    var x = Math.round(Math.random() * window.innerWidth);
+    var y = Math.round(Math.random() * window.innerHeight);
+    document.getElementById('btn').style.left = x + 'px';
+    document.getElementById('btn').style.top = y + 'px';
+}
+function yes1() {
+alert('<3');
+}
